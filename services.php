9b3bf994cdf9eacc447a4efe4bbb1e60b57494dc
nihaan62 <nihaanshah062@gmail.com>
2026-07-22 09:14:00 +0530

    Fix: services.php footer alignment - use footer-top grid class to match styles.css

--- services.php
+++ services.php
@@ -257,14 +257,12 @@ try {
 
   <footer class="site-footer">
     <div class="container container--wide">
-      <div class="footer-grid">
+      <div class="footer-top">
         <div class="footer-brand">
-          <a class="brand" href="index.php"><span class="brand-mark" aria-hidden="true"></span> THE EXPERT HUB</a>
-          <p class="footer-desc">
-            Brutalist software development and startup incubation agency. Based in Chennai, working worldwide.
-          </p>
+          <span class="brand"><span class="brand-mark" aria-hidden="true"></span> THE EXPERT HUB</span>
+          <p>Brutalist software development and startup incubation agency. Based in Chennai, working worldwide.</p>
         </div>
-        <div class="footer-links-col">
+        <div>
           <h4>Platform</h4>
           <ul>
             <li><a href="index.php">Index</a></li>
@@ -273,7 +271,7 @@ try {
             <li><a href="sales.php">Sales &amp; SMS</a></li>
           </ul>
         </div>
-        <div class="footer-links-col">
+        <div>
           <h4>Connect</h4>
           <ul>
             <li><a href="contact.php">Start a project</a></li>
