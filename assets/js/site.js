c95e97699c44eda11e6654364c554e1379e1205d
nihaan62 <nihaanshah062@gmail.com>
2026-08-09 09:26:33 +0530

    Revert: Remove Tawk.to live chat widget from site.js and all PHP pages

--- assets/js/site.js
+++ assets/js/site.js
@@ -694,15 +694,4 @@
   }
 })();
 
-/* Start of Tawk.to Script */
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-  s1.async = true;
-  s1.src = 'https://embed.tawk.to/6a77f84059ec5c1d4962a35b/1jvia3fd4';
-  s1.charset = 'UTF-8';
-  s1.setAttribute('crossorigin', '*');
-  s0.parentNode.insertBefore(s1, s0);
-})();
-/* End of Tawk.to Script */
 
